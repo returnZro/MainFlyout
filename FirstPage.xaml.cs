a4e4702228909f1e2b9e02f0e7347137ea0dd365
Rame <38007133+returnZro@users.noreply.github.com>
2024-06-07 18:58:42 -0400

    Update FirstPage.xaml.cs

--- FirstPage.xaml.cs
+++ FirstPage.xaml.cs
@@ -4,11 +4,23 @@ public partial class FirstPage : ContentPage
 {
 	public FirstPage()
 	{
-		InitializeComponent();
+	    InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+	    this.Loaded += (s, e) =>
+            {
+                this.Handler?.UpdateValue(nameof(IView.Opacity));
+            };
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-		Application.Current.MainPage.Navigation.PushAsync(new SecondPage());
+	Application.Current.MainPage.Navigation.PushAsync(new SecondPage());
+    }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+    {
+        Console.WriteLine($"##### - Detail LayoutChildren before base: {DateTime.Now}");
+        base.LayoutChildren(x, y, width, height);
+        Console.WriteLine($"##### - Detail LayoutChildren after base:{DateTime.Now}");
     }
 }
